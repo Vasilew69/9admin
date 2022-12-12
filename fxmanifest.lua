b019358da3afe9552f801793e56195a945653112
Vasilew69 <yvasilew@protonmail.com>
2022-12-12 20:59:06 +0200

    Version fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@
 author 'Vasilew'
 description 'Remotely Manage & Monitor your GTA5 FiveM Server'
 repository 'https://github.com/Vasilew69/9admin'
-version '1.0.1'
+version '1.0.0-beta.1'
 ui_label '9admin'
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
